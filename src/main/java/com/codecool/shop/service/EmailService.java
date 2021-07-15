7711d468c6d448e739c96860a05f44c41b152ef6
Dariusz Okonski <darek200180@gmail.com>
2021-07-15 01:02:08 +0100

    working on email

--- src/main/java/com/codecool/shop/service/EmailService.java
+++ src/main/java/com/codecool/shop/service/EmailService.java
@@ -7,16 +7,207 @@ import javax.mail.internet.*;
 import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.util.Properties;
 
+
+
+
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+
+
 public class EmailService implements MessageService{
     @Override
     public void sendConfirmation(String orderNumber, String incomingMessage, String incomingEmail) throws MessagingException {
+
         // email logic
         System.out.println("SENDING EMAIL: ");
 //        System.out.println("order nr: " + orderNumber);
 //        System.out.println("message: " + incomingMessage);
 //        System.out.println("to: " + incomingEmail);
+//
+
+        java_gmail_attempt();
+//        firstAttempt();
+
+    }
+
+    private void anotherOne() {
+        String to = "darek200180@gmail.com";//change accordingly
+
+        // Sender's email ID needs to be mentioned
+        String from = "darek@gmail.com";//change accordingly
+        final String username = "darek200180";//change accordingly
+        final String password = "dariuszokonski";//change accordingly
+
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "465");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class", "java.net.ssl.SSLSocketFactory");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+            // Create a default MimeMessage object.
+            Message message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(to));
+
+            // Set Subject: header field
+            message.setSubject("Testing Subject");
+
+            // Now set the actual message
+            String msg = "Some message to send";
+
+            MimeBodyPart mimeBodyPart = new MimeBodyPart();
+            mimeBodyPart.setContent(msg, "text/html");
+
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(mimeBodyPart);
 
 
+            Transport.send(message);
+
+            System.out.println("Mail successfully send");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private void another() {
+
+        // Recipient's email ID needs to be mentioned.
+        String to = "darek200180@gmail.com";//change accordingly
+
+        // Sender's email ID needs to be mentioned
+        String from = "darek200180@gmail.com";//change accordingly
+        final String username = "darek200180@gmail.com";//change accordingly
+        final String password = "dariuszokonski";//change accordingly
+
+        // Assuming you are sending email through relay.jangosmtp.net
+        String host = "smtp.gmail.com";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", "587");
+
+
+        // Get the Session object.
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+        session.getProperties().put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        session.getProperties().put("mail.smtp.starttls.enable", "true");
+
+        try {
+            // Create a default MimeMessage object.
+            Message message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(to));
+
+            // Set Subject: header field
+            message.setSubject("Testing Subject");
+
+            // Now set the actual message
+            message.setText("Hello, this is sample for to check send "
+                    + "email using JavaMailAPI ");
+
+            // Send message
+            Transport.send(message);
+
+            System.out.println("Sent message successfully....");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+
+
+    //===================================================
+    private void java_gmail_attempt() {
+        final String username = "darek200180@gmail.com";
+        final String password = "dariuszokonski";
+
+        Properties prop = new Properties();
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "587");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.starttls.enable", "true"); //TLS
+
+        Session session = Session.getInstance(prop,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("darek200180@gmail.com"));
+            message.setRecipients(
+                    Message.RecipientType.TO,
+                    InternetAddress.parse("darek200180@gmail.com")
+            );
+            message.setSubject("Testing Gmail TLS");
+            message.setText("Dear Mail Crawler,"
+                    + "\n\n Please do not spam my email!");
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
     }
 
     private void firstAttempt() throws MessagingException {
@@ -25,19 +216,16 @@ public class EmailService implements MessageService{
         Properties prop = new Properties();
         prop.put("mail.smtp.auth", true);
         prop.put("mail.smtp.starttls.enable", "true");
-//        prop.put("mail.smtp.host", "smtp.mailtrap.io");
-        prop.put("mail.smtp.host", "smtp.gmail.com");
-
+        prop.put("mail.smtp.host", "smtp.mailtrap.io");
         prop.put("mail.smtp.port", "25");
-//        prop.put("mail.smtp.ssl.trust", "smtp.mailtrap.io");
-        prop.put("mail.smtp.ssl.trust", "smtp.gmail.com");
-
-        String username = "darek";
-        String password = "pass123";
+        prop.put("mail.smtp.ssl.trust", "smtp.mailtrap.io");
 
 
-        System.out.println("=== creating session success");
+
         // creating session
+        System.out.println("=== creating session success");
+        String username = "darek200180@gmail.com";
+        String password = "dariuszokonski";
         Session session = Session.getInstance(prop, new Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
@@ -45,8 +233,10 @@ public class EmailService implements MessageService{
             }
         });
 
-        System.out.println("=== sending email success");
+
+
         // mimemessage for sending
+        System.out.println("=== sending email success");
         Message message = new MimeMessage(session);
         message.setFrom(new InternetAddress("darek200180@gmail.com"));
         message.setRecipients(
@@ -63,6 +253,6 @@ public class EmailService implements MessageService{
 
         message.setContent(multipart);
 
-        Transport.send(message);
+//        Transport.send(message);
     }
 }
