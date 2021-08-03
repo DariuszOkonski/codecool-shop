5c027f8a18f16edd7d741d06dc1ea2c080ced35e
Dariusz Okonski <darek200180@gmail.com>
2021-08-03 14:23:46 +0100

    add logout funcionality

---
+++ src/main/java/com/codecool/shop/controller/LogoutController.java
@@ -0,0 +1,19 @@
+package com.codecool.shop.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/logout"})
+public class LogoutController extends BaseController {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+//        setTemplateContext(req, resp);
+//        serviceSessionValidation(req);
+        req.getSession().invalidate();
+        resp.sendRedirect("/");
+
+    }
+}
